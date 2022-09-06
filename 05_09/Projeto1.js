d3662147a67ac93518e3709bfad1ded91ffdcfda
Thainá Kramer <thaikramer@users.noreply.github.com>
2022-09-05 21:20:51 -0300

    Atualização

---
+++ 05_09/Projeto1.js
@@ -0,0 +1,16 @@
+var nome = "Thainá"
+
+var notaDoPrimeiroBimestre = 9.869
+var notaDoSegundoBimestre = 7.856
+var notaDoTerceiroBimestre = 4.546
+var notaDoQuartoBimestre = 2.436
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+// Revisão
+// variáveis, strings. console.log, toFixed, operações matemáticas, concatenação
